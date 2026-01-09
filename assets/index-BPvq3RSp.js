e569f22ccb20997fbeda6116ba90f65bfbcc89ff
djeel <49989313+djeel@users.noreply.github.com>
2026-01-09 20:53:47 +0100

    Deploy: Update frontend 2026-01-09 20:53:47

--- assets/index-BPvq3RSp.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./index-ob7vYVjV.js";import{A as r,T as s,r as p}from"./index-ob7vYVjV.js";export{r as ActionType,s as Toaster,o as default,p as resolveValue,o as toast};
---
+++ assets/index-B_DGe-gw.js
@@ -0,0 +1,1 @@
+import{t as o}from"./index-BNwo6lo8.js";import{A as r,T as s,r as p}from"./index-BNwo6lo8.js";export{r as ActionType,s as Toaster,o as default,p as resolveValue,o as toast};
