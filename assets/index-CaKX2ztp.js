9c35484fd9979599d34a041ae3193922a04b10c1
djeel <49989313+djeel@users.noreply.github.com>
2026-01-09 21:01:26 +0100

    Deploy: Update frontend 2026-01-09 21:01:26

--- assets/index-CaKX2ztp.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./index-8vGBg9qK.js";import{A as r,T as s,r as p}from"./index-8vGBg9qK.js";export{r as ActionType,s as Toaster,o as default,p as resolveValue,o as toast};
---
+++ assets/index-Cd9S9fQj.js
@@ -0,0 +1,1 @@
+import{t as o}from"./index-DyjXkcAY.js";import{A as r,T as s,r as p}from"./index-DyjXkcAY.js";export{r as ActionType,s as Toaster,o as default,p as resolveValue,o as toast};
